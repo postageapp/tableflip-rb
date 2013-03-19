fb30c1dc22c7f1eb066e7c525a954d0ecd3ec4a8
Scott Tadman <github@tadman.ca>
2013-03-18 23:40:16 -0400

    Force encoding of query string to BINARY

--- lib/tableflip/executor.rb
+++ lib/tableflip/executor.rb
@@ -130,7 +130,7 @@ class Tableflip::Executor
 
   def do_query(db, query, *values)
     fiber = Fiber.current
-    query = query.encode(@strategy.encoding).gsub('?') do |s|
+    query = query.encode('BINARY').gsub('?') do |s|
       escaper(db, values.shift)
     end
 
